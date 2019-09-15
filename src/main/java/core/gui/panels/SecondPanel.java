2dc89ca4b86ae1b903592f7ae2760e28e7b692a0
AbgarSim <abgar1223@gmail.com>
2019-09-15 13:24:09 +0300

    Added table

--- src/main/java/core/gui/panels/SecondPanel.java
+++ src/main/java/core/gui/panels/SecondPanel.java
@@ -20,6 +20,7 @@ public class SecondPanel extends JPanel {
     private JTextField from;
     private JTextField to;
     private JComboBox<String> dropDown;
+    private JTable dbTable;
 
     public SecondPanel(JFrame parent) {
         //parent
@@ -30,18 +31,31 @@ public class SecondPanel extends JPanel {
         //construct preComponents
         String[] dropDownItems = {"Item 1", "Item 2", "Item 3"};
 
+
+
+        // Column Names
+        String[] columnNames = {"Name", "Roll Number", "Department"};
+
+        // Data to be displayed in the JTable
+        String[][] data = {
+                columnNames,
+                {"Kundan Kumar Jha", "4031", "CSE"},
+                {"Anand Jha", "6014", "IT"}
+        };
+
         //construct components
-        titleLabel = new JLabel ("Фильмы");
-        addButton = new JButton ("Добавить");
-        checkBox = new JCheckBox ("Доступны субтитры");
-        priceLabel = new JLabel ("Цена");
-        from = new JTextField (5);
-        to = new JTextField (5);
+        titleLabel = new JLabel("Фильмы");
+        addButton = new JButton("Добавить");
+        checkBox = new JCheckBox("Доступны субтитры");
+        priceLabel = new JLabel("Цена");
+        from = new JTextField(5);
+        to = new JTextField(5);
         dropDown = new JComboBox<String>(dropDownItems);
+        dbTable = new JTable(data, columnNames);
 
         //adjust size and set layout
-        setPreferredSize (new Dimension(714, 447));
-        setLayout (null);
+        setPreferredSize(new Dimension(714, 447));
+        setLayout(null);
 
         //add listeners
         addButton.addActionListener(
@@ -50,22 +64,24 @@ public class SecondPanel extends JPanel {
                 ));
 
         //add components
-        add (titleLabel);
-        add (addButton);
-        add (checkBox);
-        add (priceLabel);
-        add (from);
-        add (to);
-        add (dropDown);
+        add(titleLabel);
+        add(addButton);
+        add(checkBox);
+        add(priceLabel);
+        add(from);
+        add(to);
+        add(dropDown);
+        add(dbTable);
 
         //set component bounds (only needed by Absolute Positioning)
-        titleLabel.setBounds (15, 30, 120, 30);
-        addButton.setBounds (150, 30, 105, 30);
-        checkBox.setBounds (270, 30, 150, 30);
-        priceLabel.setBounds (430, 30, 55, 30);
-        from.setBounds (485, 35, 30, 25);
-        to.setBounds (525, 35, 30, 25);
-        dropDown.setBounds (580, 35, 100, 25);
+        titleLabel.setBounds(15, 30, 120, 30);
+        addButton.setBounds(150, 30, 105, 30);
+        checkBox.setBounds(270, 30, 150, 30);
+        priceLabel.setBounds(430, 30, 55, 30);
+        from.setBounds(485, 35, 30, 25);
+        to.setBounds(525, 35, 30, 25);
+        dropDown.setBounds(580, 35, 100, 25);
+        dbTable.setBounds(45, 80, 635,300);
 
         //adjust alignments
         titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
